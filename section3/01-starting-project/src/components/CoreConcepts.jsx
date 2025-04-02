4c2302f59e7c3374c467bce5486ce48bcd7886a4
BrianCorella <briancorella@gmail.com>
2025-04-01 21:53:06 -0600

    Video48

---
+++ section3/01-starting-project/src/components/CoreConcepts.jsx
@@ -0,0 +1,9 @@
+export default function CoreConcepts({ title, description, image }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </li>
+  );
+}
